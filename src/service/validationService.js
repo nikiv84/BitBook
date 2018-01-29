17116876fa51defb16e6e7544cd8ff0f345ef63c
Ivan Nikolic <i.nikolic84po@gmail.com>
2018-01-29 04:34:29 +0100

    Some changes

--- src/service/validationService.js
+++ src/service/validationService.js
@@ -49,37 +49,37 @@ class ValidationService {
         if (!this.hasAllRequiredFields(data)) {
             errors.allFields = "All fields must be filled out!";
             callback(errors);
-
+            return false;
         }
 
         if (!this.isNameValid(data)) {
             errors.name = "Name must contain first and last name!";
             callback(errors);
-            // return false;
+            return false;
         }
 
         if (!this.isUsernameValid(data)) {
             errors.username = "Username must be longer than 3 characters!";
             callback(errors);
-            // return false;
+            return false;
         }
 
         if (!this.isEmailValid(data)) {
             errors.email = "Email is not in valid format!";
             callback(errors);
-            // return false;
+            return false;
         }
 
         if (!this.isPasswordValid(data)) {
             errors.password = "Password must be longer than 6 characters!";
             callback(errors);
-            // return false;
+            return false;
         }
 
         if (!this.isPasswordConfirm(data)) {
             errors.repeatPassword = "Passwords must match!";
             callback(errors);
-            // return false;
+            return false;
         }
 
         if (Object.keys(errors).length !== 0) {
@@ -163,7 +163,7 @@ class ValidationService {
     }
 
     isPasswordConfirm(data) {
-        if ((data.password != data.repeatPassword) && data.repeatPassword !== "") {
+        if ((data.password !== data.repeatPassword) && data.repeatPassword !== "") {
             return false;
         }
         return true;
